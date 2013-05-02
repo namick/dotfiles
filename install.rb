998fb0dd07436ea55956a3730cf8498fdde07bd8
Nathan Amick <github@nathanamick.com>
2013-05-02 15:50:28 -0700

    Refactor bundles to seperate file, add tmux.conf

--- install.rb
+++ install.rb
@@ -14,3 +14,6 @@ dot_files.each do |filename|
   FileUtils.rm sym_link if File.symlink?(sym_link) || File.exist?(sym_link)
   FileUtils.ln_s filename,sym_link
 end
+
+`git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle`
+`vim -u ~/.vimrc.bundles +BundleInstall +qa`
