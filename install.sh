4e5e8d79035670b012d7893046cab4c7a31c6b63
Nathan Amick <github@nathanamick.com>
2014-07-03 14:24:58 -0700

    Build command-T

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ rm -rf ~/.vim/bundle
 mkdir ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -u ~/.vimrc.bundles +BundleInstall +qa
+
+cd ~/.vim/bundle/Command-T/ruby/command-t/
+ruby extconf.rb
+make
