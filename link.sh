f392e19a870b9b2ed3c6c57ed547dcd6baac0cc1
Nathan Amick <github@nathanamick.com>
2015-10-07 10:37:37 -0500

    Clean up tmux.conf and links

--- link.sh
+++ link.sh
@@ -23,18 +23,25 @@ create_link 'gitignore' '.gitignore'
 create_link 'gemrc' '.gemrc'
 
 create_link 'tmux.conf' '.tmux.conf'
+if [[ "$(uname)" = "Darwin" ]]; then
+  create_link 'tmux-osx.conf' '.tmux-osx.conf'
+fi
+
 create_link 'ackrc' '.ackrc'
 
 create_link 'vimrc' '.vimrc'
 mkdir -vp $HOME/.vim/backup
 mkdir -vp $HOME/.vim/bundle
 
+# Install Vundle
 if [[ ! -d ${HOME}/.vim/bundle/Vundle.vim ]]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 
-vim +PluginInstall! +qall
+# Install colors early so Vim doesn't complain
+if [[ ! -d ${HOME}/.vim/bundle/vim-vividchalk ]]; then
+  git clone https://github.com/tpope/vim-vividchalk.git $HOME/.vim/bundle/vim-vividchalk
+fi
 
-# for bin in bin/*; do
-# 	create_link "$bin" ".local/${bin}"
-# done
+vim +PluginInstall +qall
+
