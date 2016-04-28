b8364fa884ea37f73e9056a39b9b048a98e14b2d
Nathan Amick <github@nathanamick.com>
2016-04-27 21:41:12 -0500

    Update install-rbenv script

--- install-rbenv.sh
+++ install-rbenv.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# You might need this on debian based os's
+# apt-get update
+# apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+# Install rbenv
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 
+# Install ruby-build plugin
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
 
+# Install plugin to auto run rbenv rehash
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
+rbenv install 2.2.3
+rbenv global 2.2.3
+ruby -v
 
